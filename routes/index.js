52db769c7bb630202948c3fcf28a0c393ac453e9
sammykariuki <sammykariuki099@gmail.com>
2024-06-07 11:06:37 +0300

    Books index, new and create routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const Book = require('../models/book')
 
-router.get('/', (req, res) => {
-    res.render('index')
+router.get('/', async (req, res) => {
+    let books = []
+    try{
+        books = await Book.find().sort({createdAt: -1}).limit(10).exec()
+    } catch (error){
+        console.error('Error in retrieving and sorting recently added books: ', error)
+        books = []
+    }
+    res.render('index', {books: books})
 })
 
 module.exports = router
